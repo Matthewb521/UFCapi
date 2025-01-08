5bd978cf59ee32f1c4c216893a9a4477c6ef5c7a
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2025-01-08 12:25:08 -0500

    a

--- app/main.js
+++ app/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+async function getData() {
+  const response = await fetch(
+    "https://ufc-api-theta.vercel.app/mma-api/fighters?"
+  );
+  const data = await response.json();
+  console.clear();
+  console.log(data);
+}
+getData();
