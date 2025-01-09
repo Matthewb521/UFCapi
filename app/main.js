7a829206f665d6cd0fa2fe9ae8869696a5fb6a83
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2025-01-09 12:01:18 -0500

    a

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 async function getData() {
   const response = await fetch(
-    "https://ufc-api-theta.vercel.app/mma-api/fighters?"
+    "https://ufc-api-theta.vercel.app/mma-api/fighters?name=Johnny Walker"
   );
   const data = await response.json();
   console.clear();
